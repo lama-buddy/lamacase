abd3f68e4d78ae90f5a0b47ec2ea51d2bd43a18a
Lama Chandrasena <lama@buddy.com>
2016-09-16 15:37:45 +0930

    updated module

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 
 const camelCase = require('camelcase');
 
-module.exports = {
-  convert: function(inputString) {
-    return camelCase(inputString);
-  }
+// module.exports = {
+//   convert: function(inputString) {
+//     return camelCase(inputString);
+//   }
+// }
+
+exports.convert = function(inputString) {
+  return camelCase(inputString);
 }
